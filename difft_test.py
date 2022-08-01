a0599c28bc707c750ce8b70ef50531615dc0bfd8
Seungho Lee <jimmylee871124@gmail.com>
2022-08-01 16:18:31 +0900

    implement string to json

--- difft_test.py
+++ difft_test.py
@@ -8,11 +8,22 @@ r = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.
 output, error = r.communicate()
 joutput = json.dumps(output.decode('utf-8'), separators=(',',':'))
 data = json.loads(joutput)
-s2 = re.sub('\s', '', output)
+# s2 = re.sub('\s', '', data)
 # Surrounding any word with "
-s3 = re.sub('(\w+)', '"\g<1>"', s2)
-# print(s2)
-print(output)
-# print(data)
+# s3 = re.sub('(\w+)', '"\g<1>"', data)
+# s3 = re.sub('\{[ \t]+[^\"]*[a-zA-Z]+[^\"][ \t]+:', '"\g<1>"', data)
+s1 = re.sub('(\w+ id(?=:))', '"\g<1>"', data)
+s2 = re.sub('((?<=:)\w+(?= ))','"\g<1>"', s1)
+s3 = re.sub('((?<= )\w+(?=:))','"\g<1>"', s2)
+
+with open('./test.json', 'w') as fout:
+    json.dump(s3, fout)
+    
+with open('./test.json', 'r') as read_file:
+    data = json.load(read_file)
+
+# print(s3)
+# jdata = json.loads(s3.replace("'", "\""))
+# print(jdata)
 # fd = os.open('difft_test.output',os.O_RDWR|os.O_CREAT)
 # os.write(fd, data)
