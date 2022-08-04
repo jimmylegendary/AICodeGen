80c41802ff0b7891ab8bc2e4b8a9c996e7ba8b6c
Seungho Lee <jimmylee871124@gmail.com>
2022-08-04 16:24:38 +0900

    modify for str to json

--- difft_test.py
+++ difft_test.py
@@ -12,18 +12,32 @@ data = json.loads(joutput)
 # Surrounding any word with "
 # s3 = re.sub('(\w+)', '"\g<1>"', data)
 # s3 = re.sub('\{[ \t]+[^\"]*[a-zA-Z]+[^\"][ \t]+:', '"\g<1>"', data)
-s1 = re.sub('(\w+ id(?=:))', '"\g<1>"', data)
-s2 = re.sub('((?<=:)\w+(?= ))','"\g<1>"', s1)
-s3 = re.sub('((?<= )\w+(?=:))','"\g<1>"', s2)
+s0 = re.sub('((?<=[0-9] )(?={))','\g<1>"body":\n', data)
+s0 = re.sub('(Atom id)', '{\n"\g<1>"', s0)
+s0 = re.sub('(},)', '\g<1>\n},', s0)
+s1 = re.sub('(\w+ id(?=:))', '"\g<1>"', s0)
+s2 = re.sub('((?<=:)\w+(?= ))','"\g<1>",', s1)
+s3 = re.sub('((?<= )\w+(?=: ))','"\g<1>"', s2)
+s3 = re.sub('(content(?=:))','"\g<1>"', s3)
 
-with open('./test.json', 'w') as fout:
-    json.dump(s3, fout)
-    
-with open('./test.json', 'r') as read_file:
-    data = json.load(read_file)
 
+print(s3)
+# print(tdata[0:39])
+# jdata = json.loads(s3)
+# s3 = re.sub('\s','', s3)
 # print(s3)
+
+# with open('./test.json', 'w') as fout:
+#     json.dump(s3, fout)
+    
+# with open('./test.json', 'r') as read_file:
+#     data = json.load(read_file)
+# print(s3)
+# data = json.loads(s3)
+# print(data[0:500])
+# print(s3[0:])
+# print(data)
 # jdata = json.loads(s3.replace("'", "\""))
 # print(jdata)
-# fd = os.open('difft_test.output',os.O_RDWR|os.O_CREAT)
+# fd = os.open('test.json',os.O_RDWR|os.O_CREAT)
 # os.write(fd, data)
