9426944df5bee6e97d480b552790d75b154b2bec
Seungho Lee <jimmylee871124@gmail.com>
2022-09-07 05:15:02 +0000

    modify patch_data.py by data structure changes

--- patch_data.py
+++ patch_data.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+from util import RepoData
+
 class CodeReviewDataManager:
     def __init__(self, path):
         self.data_list : list[CodeReviewData] = []
@@ -27,58 +29,52 @@ class CodeReviewData:
 
 data_manager = CodeReviewDataManager(path='./')
 
-def get_patch_dict(patch_filename, msg_filename):
-    # file = open(filename,'rt', encoding='UTF-8')
-    # tree = parser.parse(bytes(file.read(),"utf8"))
-    # file = open(filename,'rt')
-    
-    msg = ''
-    with open(msg_filename,'r') as f:
-        msg = f.read()
-        f.close()
-    
-    with open(patch_filename,'r') as f:
-        count = 0
-        lines = f.readlines()
-        lines = [line for raw_line in lines for line in raw_line.split('\n')]
-        lines = list(filter(len, lines))
-        for idx in range(len(lines)):
-            if lines[idx].startswith('@@'):
-                count += 1
-                old_patch_str = ''
-                new_patch_str = ''
-                while idx < len(lines) - 1:
-                    idx += 1
-                    if lines[idx].startswith('@@'):
-                        idx -= 1
-                        break
-                    lines[idx] = lines[idx] + '\n'
-                    if lines[idx][0] == '-':
-                        old_patch_str += lines[idx]
-                    elif lines[idx][0] == '+':
-                        new_patch_str += lines[idx]
-                    else:
-                        old_patch_str += lines[idx]
-                        new_patch_str += lines[idx]
-                data_manager.data_list.append(CodeReviewData(
-                    old=old_patch_str, 
-                    new=new_patch_str,
-                    msg=msg
-                    )
+def get_patch_dict(patch, msg):
+    count = 0
+    lines = [line for line in patch.split('\n')]
+    lines = list(filter(len, lines))
+    for idx in range(len(lines)):
+        if lines[idx].startswith('@@'):
+            count += 1
+            old_patch_str = ''
+            new_patch_str = ''
+            while idx < len(lines) - 1:
+                idx += 1
+                if lines[idx].startswith('@@'):
+                    idx -= 1
+                    break
+                lines[idx] = lines[idx] + '\n'
+                if lines[idx][0] == '-':
+                    old_patch_str += lines[idx]
+                elif lines[idx][0] == '+':
+                    new_patch_str += lines[idx]
+                else:
+                    old_patch_str += lines[idx]
+                    new_patch_str += lines[idx]
+            data_manager.data_list.append(CodeReviewData(
+                old=old_patch_str, 
+                new=new_patch_str,
+                msg=msg
                 )
+            )
 
-root_dirt = 'pair_data'
-for i in range(45):
-    root_dir = 'pair_data/' + str(i)
-    for (root, dirs, files) in os.walk(root_dir):
-        if len(files) > 0 and 'patch' in files and 'msg' in files:
-            for file_name in files:
-                if file_name == 'patch':
-                    patch_file = f'{root}/{file_name}'
-                elif file_name == 'msg':
-                    msg_file = f'{root}/{file_name}'
-            get_patch_dict(patch_file, msg_file)
-        
+from types import SimpleNamespace
+json_file = 'test.jsonl'
+json_data = []
+for line in open(json_file):
+    try:
+        json_data.append(json.loads(line, object_hook=lambda d:SimpleNamespace(**d)))
+    except:
+        continue
+
+repo_data_list = []
+for data in json_data:
+    for keyword_data in data.keyword_data_list:
+        for commit_data in keyword_data.commit_data_list:
+            patch = commit_data.patch
+            msg = commit_data.msg
+            get_patch_dict(patch, msg)
+
 # for i, data in enumerate(data_manager.data_list):
 #     print(f'### {i}')
 #     print('old')
